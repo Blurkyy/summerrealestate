dd2204236c0fc4f730cf3cd5440b70dfb98a2f7a
Blurkyy <michaelmesfin202@gmail.com>
2026-07-13 20:23:18 +0300

    AD

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${fraunces.variable} ${inter.variable}`}>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18302367953"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-18302367953');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
